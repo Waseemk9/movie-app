34126c1ee698ae57b3223651892d1c2917b252e1
Waseem <shonu.waseem786@gmail.com>
2023-11-01 12:39:48 -0500

    Added SignUp and Login Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Home from './Components/Pages/Home/Home';
+import LoginSignup from './Components/Pages/LoginSignup/LoginSignup';
 
 function App() {
   return (
@@ -8,8 +10,8 @@ function App() {
       <Router>
       <Header />
         <Routes>
-          <Route index element= {<h1>Movie App</h1>}></Route>
-          <Route path="movie/login" element={ <h1> Login Page</h1>}></Route>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/login" element={<LoginSignup />}></Route>
           <Route path="movie/search" element={ <h1> Search</h1>}></Route>
           <Route path="movie/movies" element={ <h1> Movies details page</h1>}></Route>
           <Route path="/*" element= {<h1> Error Page </h1>} ></Route>
---
+++ src/Components/Pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <>Home page is now rendering</>
+    )
+}
+
+export default Home
